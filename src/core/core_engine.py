f5f456fbf0bcdf35b010f9a18709391a58cf8a1c
alejocp00 <alex00cu@gmail.com>
2024-10-16 21:39:40 -0400

    finished first implementation of core engine

--- src/core/core_engine.py
+++ src/core/core_engine.py
@@ -4,16 +4,21 @@ class CoreEngine():
     
     def __init__(self,config:cfg.Config) -> None:
         self._config = config
-        self._log = {{}}
+        # self._log = {{}}
 
     @property
     def config(self)->cfg.Config:
         return self._config
 
     def start_tournament(self) -> None:
-        tournament = cfg.Tournament(self._config.tournament_engine, self._config.game)
+        # Todo: tomar el id del torneo de algún lugar
+        tournament = cfg.Tournament(0,self._config.tournament_engine, self._config.game)
+        tournament.players = self._config.players_in_tournament
         
         for match in tournament:
+            print(match)
             for action in match:
                 # self._log[tournament.id][match.game_id] = action
                 print(action)
+        
+        print(f"The Winner is: {tournament.get_winner()}")
